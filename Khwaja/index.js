9ced59d4f6e4687e31931f0cb507a042f72108f9
khwajapasha <khwajapasha141@gmail.com>
2023-01-29 15:00:07 +0530

    KP-Added dynamic dice and dice audio

--- Khwaja/index.js
+++ Khwaja/index.js
@@ -1,11 +1,10 @@
 let diceImage = document.getElementById("diceImage");
 let rollDice = document.getElementById("rollDice");
+let diceAudio = document.getElementById("audio");
 var num = null;
 rollDice.addEventListener("click", () => {
   num = Math.ceil(Math.random() * 6);
   console.log(num);
-  diceImage.setAttribute(
-    "src",
-    `https://thumbs.dreamstime.com/b/dice-18514485.jpg`
-  );
+  diceImage.setAttribute("src", `../Assets/Images/dice${num}.png`);
+  diceAudio.play();
 });
