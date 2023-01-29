29587fae2ec2bb13acd06744d67030a44c2887bf
khwajapasha <khwajapasha141@gmail.com>
2023-01-29 13:35:37 +0530

    KP-Learned Math Object

--- Github/index.js
+++ Github/index.js
@@ -1,5 +1,6 @@
 let containerDiv = document.querySelector(".container");
 let btn = document.querySelector("#myBtn");
+var x = document.getElementById("input");
 
 // with event listner
 btn.addEventListener("click", () => {
@@ -11,6 +12,8 @@ btn.addEventListener("click", () => {
 const changeColor = (value) => {
   if (value === "add") {
     console.log("=====>>>>>");
+    console.log(x);
+    containerDiv.innerHTML = x.value;
     containerDiv.classList.add("red");
   } else if (value === "remove") {
     containerDiv.classList.remove("red");
@@ -18,3 +21,8 @@ const changeColor = (value) => {
     containerDiv.classList.toggle("hidden");
   }
 };
+
+// var k = document.createElement("p");
+// // var a = (containerDiv.innerHTML = document.createElement("p"));
+// k.innerHTML = "I am para";
+// containerDiv.append(k);
---
+++ Math/index.js
@@ -0,0 +1,22 @@
+// console.log(Math.round(72.1));
+// console.log(Math.ceil(72.2)); one number up
+// console.log(Math.floor(72.2));
+// console.log(Math.trunc(Math.random()));
+
+// const rollDice = () => {
+//   //   console.log(Math.ceil(Math.random() * 6));
+//   var num = Math.ceil(Math.random() * 20);
+//   console.log(num);
+// };
+
+// console.log(Math.sign(-10)); //checks provided value is positive or negative
+// console.log(Math.sign(10));
+// console.log(Math.pow(2, 2));// return the power of given number
+// Math.sqrt(144);// to retuen the square root
+// console.log(Math.min(0, 150, 30, 20, -8, -200));// return minimum num
+// console.log(Math.max(0, 150, 30, 20, -8, -200));// return maximum number
+
+// RegExp
+// const phonenumber = /^[\+]?[(]?[0-9]{3}[)]?[-\s\]?[0-9]{3}[-\s\]?[0-9]{4,6}$/im;
+// const email =
+//   /^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\. [a-zA-Z0-9-]+)*$/;
