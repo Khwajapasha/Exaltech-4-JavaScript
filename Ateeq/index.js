399505a5b5c8bb17d4212de1d1905e382de7d853
abdulateeq <abdulateeq682@gmail.com>
2023-01-30 00:31:50 +0530

    chaush 119

--- Ateeq/index.js
+++ Ateeq/index.js
@@ -1,1 +1,8 @@
-console.log("hello");
+// console.log("hello");
+
+var button = document.getElementById("btn")
+
+button.addEventListener ("click", function (){
+    var chaush = Math.ceil(Math.random()*5)
+    console.log(chaush);
+})
