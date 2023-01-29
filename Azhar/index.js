6121ec92d1b1b2b9cbbdd730fb89bbb97862ee21
azharhussaian <azharhn643@gmailcom>
2023-01-29 15:15:20 +0530

    AHN:-Onclick Number Changing

--- Azhar/index.js
+++ Azhar/index.js
@@ -1,4 +1,11 @@
- var azhar = Math.ceil(Math.random() * 10);
-console.log(azhar);
+var button = document.getElementById("btn")
+// console.log(button);
+
+button.addEventListener("click",function(){
+    var azhar = Math.ceil(Math.random() * 10);
+    console.log(azhar);
+
+})
+
 
 
