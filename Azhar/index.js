c9ba5255a022ee3b97723dc5a1c09175e03801a8
azharhussaian <azharhn643@gmailcom>
2023-01-30 22:29:42 +0530

    AHN:-Added Math Object Properties.

--- Azhar/index.js
+++ Azhar/index.js
@@ -1,13 +1,31 @@
-var button = document.getElementById("btn")
+// var button = document.getElementById("btn")
 
 
 // console.log(button);
 
-button.addEventListener("click",function(){
-    var azhar = Math.ceil(Math.random() * 10);
-    console.log(azhar);
+// button.addEventListener("click",function(){
+//     var azhar = Math.ceil(Math.random() * 10);
+//     console.log(azhar);
 
-})
+// })
+// ******************************************************************************************************************
+// ****************************************** MATH OBJECT PTOPERTY **************************************************
+// ******************************************************************************************************************
 
+// 1:- Math.round                               [It Reaturns The Roundup Value Of Given Number.]
+console.log(Math.round(76.20));
+console.log(Math.round(57.60))
 
+// 2:- Math.ceil                                [It Returns The Upper Value Of Given Number.]
+console.log(Math.ceil(25.60));
+console.log(Math.ceil(25.40))
 
+// 3:- Math.floor                               [It Returns The Lower Value Of Given Number.]   [Math.floor == Math.trunc]
+console.log(Math.floor(30.99));
+console.log(Math.floor(30.10));
+
+// 4:- Math.trunc                               [It Returns Number Which Comes Before The "."(Dot) Of Given Number.]
+console.log(Math.trunc(20.40));
+console.log(Math.trunc(20.99));
+
+// 5:- Math.
