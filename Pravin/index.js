552c476def1605eb38a5f5f9a272aab53460b3f8
Pravin876 <pravinsardar783@gmail.com>
2023-01-30 10:03:56 +0530

    rollDice

--- Pravin/index.js
+++ Pravin/index.js
@@ -3,7 +3,8 @@ let diceImage = document.getElementById("diceImage");
 
 rollDice= () => {
   
-  num= Math.ceil(Math.random() * 6) ;
+  num= Math.ceil(Math.random() * 6) ; 
   console.log(num);diceImage.setAttribute("src", `../Assets/Images/dice${num}.png`);
   
   };
+  
